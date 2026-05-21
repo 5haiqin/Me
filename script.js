2d3eab2e8b870908f407a7e4674543b728f7a81b
5haiqin <5haiqin.tech@gmail.com>
2026-05-21 15:04:33 +0530

    feat: configure tech logos loop for edcare tutorial card

--- script.js
+++ script.js
@@ -273,3 +273,5 @@ document.addEventListener('DOMContentLoaded', () => {
 // 2026-05-15: feat: convert project page cards to standard homepage card style
 
 // 2026-05-18: feat: map bootstrap badges on netflix clone card sidebar
+
+// 2026-05-21: feat: configure tech logos loop for edcare tutorial card
